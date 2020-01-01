6f5893dc2a90bc710132299296b09ec2e5fd46be
UrielAbergel <myfliper2@gmail.com>
2020-01-01 11:18:31 +0200

    update the Files

---
+++ recv_udp.c
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdio.h>
+ 
+/*
+void printsin(struct sockaddr_in *s, char *str1, char *str2) {
+  printf("%s\n", str1);
+  printf("%s: ", str2);
+  -- port: sin->sin_port (host integer type) 
+  -- IP: sin->sin_addr (IP in dotted-decimal notation) 
+  printf("\n");
+}
+*/
+ 
+int main(int argc, char *argv[])
+{
+  /*define the socket id and the size of the socket*/
+  int socket_fd, cc, fsize;
+ /*define struct that will be the socket*/
+  struct sockaddr_in  s_in, from;
+   /*define struct that will be the the messege in the messenger*/
+  struct { char head; u_long  body; char tail;} msg;
+ 
+
+/*Define socket IPv4 of this protocol ,UDP in the type, and protocol and send zero becuase this is defualt */
+  socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
+ 
+  bzero((char *) &s_in, sizeof(s_in));  /* They say you must do this    */
+/*define the socket addres that he will communicted later*/
+  s_in.sin_family = (short)AF_INET;
+  s_in.sin_addr.s_addr = htonl(INADDR_ANY);    /* WILDCARD */
+  s_in.sin_port = htons((u_short)0x3333);
+ 
+  //printsin( &s_in, "RECV_UDP", "Local socket is:"); 
+  fflush(stdout);
+ 
+  /*define the socket addres that he will communicted later*/
+  bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in));
+ 
+  /*infinty loop for the working soucket*/
+  for(;;) {
+    fsize = sizeof(from);
+     /*take the messege and put it in the messge storge */
+    cc = recvfrom(socket_fd,&msg,sizeof(msg),0,(struct sockaddr *)&from,&fsize);
+    //printsin( &from, "recv_udp: ", "Packet from:");
+    printf("Got data ::%c%ld%c\n",msg.head,(long) ntohl(msg.body),msg.tail); 
+    fflush(stdout);
+  }
+   /*take a socket and connect it to the adress */
+  return 0;
+}
